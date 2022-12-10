4da7440d1aab3fa6fffa626970801bdbe4f2fc2d
phantomreaper-099 <amanprathapvarma@gmail.com>
2022-12-10 19:38:48 +0530

    Update Files

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,57 +3,72 @@
 -- │░▀▄▀░░█░░█░█│
 -- │░░▀░░▀▀▀░▀░▀│
 -- └────────────┘
-vim.o.ruler = true
-vim.o.number = true
-vim.o.showcmd = true
-vim.o.autoindent = true
-vim.o.wrap = false
-vim.o.ignorecase = true
-vim.o.incsearch = true
-vim.o.tabstop = 5
-vim.o.softtabstop = 5
-vim.o.wildmode = 'longest,list'
-vim.o.shiftwidth = 5
-vim.o.mouse = 'a'
-vim.o.list = true
-vim.o.listchars = 'tab:>>'
-vim.o.cursorline = true
-vim.o.tgc = true
-
--- Packages
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
- Plug('dracula/vim', {as = 'dracula'})
- Plug('ms-jpq/chadtree', {branch = 'chad', ['do'] = vim.fn['python3 -m chadtree deps']})
+ Plug('dracula/vim', { as = 'dracula' })
+ Plug('ms-jpq/chadtree', { branch = 'chad', ['do'] = vim.fn['python3 -m chadtree deps'] })
  Plug 'elkowar/yuck.vim'
  Plug 'mhinz/vim-startify'
  Plug 'nvim-lualine/lualine.nvim'
- Plug('ms-jpq/coq_nvim', {branch = 'coq'})
- Plug 'ms-jpq/coq.artifacts'
- Plug 'ms-jpq/coq.thirdparty'
- Plug('rrethy/vim-hexokinase', {['do'] = 'make hexokinase' })
+ Plug('rrethy/vim-hexokinase', { ['do'] = 'make hexokinase' })
  Plug 'neovim/nvim-lspconfig'
+ Plug 'hrsh7th/nvim-cmp'
+ Plug 'hrsh7th/cmp-nvim-lsp'
+ Plug 'hrsh7th/cmp-path'
+ Plug 'hrsh7th/cmp-buffer'
+ Plug 'hrsh7th/cmp-cmdline'
+ Plug 'hrsh7th/cmp-nvim-lua'
+ Plug 'saadparwaiz1/cmp_luasnip'
 vim.call('plug#end')
 
-vim.cmd('colorscheme dracula')
-vim.cmd('highlight MatchParen guifg=#21222c')
-vim.cmd('highlight MatchParen guibg=#6272a4')
-vim.g.startify_lists = {{ type = 'dir', header = {'   MRU : /home/aman'}},}
+-- Settings
+vim.o.ru = true --ruler
+vim.o.nu = true --number
+vim.o.sc = true --showcmd
+vim.o.ai = true --autoindent
+vim.o.wrap = false
+vim.o.ic = true --ignorecase
+vim.o.is = true --incsearch
+vim.o.ts = 4 --tabstop
+vim.o.sts = 4 --softtabstop
+vim.o.sw = 5 --shiftwidth
+vim.o.mouse = 'a'
+vim.o.list = true
+vim.o.lcs = 'tab:>-,multispace:-+,' --listchars
+vim.o.cul = true --cursorline
+vim.o.culopt = "both" --cursorlineopt
+vim.o.tgc = true --termguicolors
+vim.o.cot = 'menu,menuone,noselect' --completeopt
+vim.cmd('colo dracula')
+vim.cmd('hi MatchParen guifg=#21222c')
+vim.cmd('hi MatchParen guibg=#6272a4')
+vim.opt.gfn = { "JetBrainsMono Nerd Font", ":h8" } --guifont
+vim.g.neovide_cursor_vfx_mode = "wireframe"
+vim.cmd('map <C-C> "+y')
+vim.cmd('map <C-V> "+gP')
+vim.cmd('map <C-F> :CHADopen --nofocus<Enter>')
+
+-- Packages
+vim.cmd('autocmd VimEnter * CHADopen --nofocus')
+vim.cmd('autocmd BufEnter * if (winnr("$") == 1 && &buftype == "nofile" && &filetype == "CHADTree") | q! | endif')
+vim.g.startify_lists = {
+	 { type = 'bookmarks', header = {'\tBookmarks : '}}}
+vim.g.startify_bookmarks = { 
+	 '~/.config/nvim/init.lua', 
+	 '~/.config/qtile/config.py',
+	 '~/.config/rofi/config.rasi',
+	 '~/.config/eww/eww.yuck' }
 vim.g.startify_custom_header = {
 '	┌────────────────────────┐',
 '	│║ ║╝╔╔   ╝╔═╝  ╔═╝╔═║╔═ │',
 '	│║ ║║║║║  ║══║  ║ ║║ ║║ ║│',
 '	│ ╝ ╝╝╝╝  ╝══╝  ══╝══╝══ │',
 '	└────────────────────────┘'}
-vim.g.startify_custom_footer = {
-"	Software is like sex, it's better when it's free",
-'		- Linus Torvalds',}
-vim.cmd('autocmd VimEnter * CHADopen --nofocus')
-vim.cmd('autocmd BufEnter * if (winnr("$") == 1 && &buftype == "nofile" && &filetype == "CHADTree") | q! | endif')
+vim.g.startify_custom_footer = 'startify#pad(startify#fortune#cowsay())'
 vim.g.chadtree_settings = {
 	['options.show_hidden'] = true,
 	['theme.icon_glyph_set'] = "devicons",
-	['theme.icon_colour_set'] = "github",
+	['theme.icon_colour_set'] = "none",
 	['theme.text_colour_set'] = "nord",
 	['theme.discrete_colour_map'] = {
 		blue = '#bd93f9',
@@ -67,22 +82,49 @@ vim.g.chadtree_settings = {
 vim.g.Hexokinase_highlighters = {'backgroundfull'}
 vim.g.Hexokinase_optInPatterns = 'full_hex,rgb,rgba,hsl,hsla'
 
--- LSP
-require('lspconfig').pyright.setup{}
-require("coq_3p") {
-  { src = "nvimlua", short_name = "nLUA" },
-  { src = "vimtex",  short_name = "vTEX" },
-  { src = 'builtin/syntax' },
+-- LSP and autocomplete
+local cmp = require'cmp'
+cmp.setup({
+	 snippet = {
+		expand = function(args)
+		require('luasnip').lsp_expand(args.body)
+	 end,
+	 },
+	 experimental = { ghost_text = true },
+	 window = { 
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered()
+	 },
+	 mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	 }),
+	 sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+	 }, {
+		{ name = 'nvim_lua' },
+	 })
+})
+cmp.setup.cmdline(':', {
+	 mapping = cmp.mapping.preset.cmdline(),
+	 sources = cmp.config.sources({
+		  { name = 'path' }
+	 }, {
+		  { name = 'cmdline' }
+	 })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('lspconfig')['pyright'].setup {
+	 capabilities = capabilities
 }
-vim.g.coq_settings = {
-	['display.icons.mode'] = "short",
-	['auto_start'] = 'shut-up',
-	['clients.lsp.enabled'] = true,
-	['clients.buffers.enabled'] = false}
 
 -- Lualine
 require('lualine').setup {
-options = {
+ options = {
 	icons_enabled = true,
 	theme = require('lualine.themes.dracula'),
 	component_separators = { left = '', right = ' '},
@@ -99,25 +141,25 @@ options = {
 		tabline = 1000,
 		winbar = 1000,
 	}
-},
-sections = {
+ },
+ sections = {
 	lualine_a = {{'mode',icon={'',align='right'}}},
 	lualine_b = {'branch','diff','diagnostics'},
 	lualine_c = {'filename'},
 	lualine_x = {'encoding','fileformat'},
 	lualine_y = {'filetype'},
 	lualine_z = {'location'}
-},
-inactive_sections = {
+ },
+ inactive_sections = {
 	lualine_a = {},
 	lualine_b = {},
 	lualine_c = {'filename'},
 	lualine_x = {'location'},
 	lualine_y = {},
 	lualine_z = {}
-},
-tabline = {},
-winbar = {},
-inactive_winbar = {},
-extensions = {'chadtree'}
+ },
+ tabline = {},
+ winbar = {},
+ inactive_winbar = {},
+ extensions = {'chadtree'}
 }
--- qtile/config.py
+++ qtile/config.py
@@ -3,10 +3,10 @@
 # │░█\█░░█░░░█░░█░░░█▀▀│
 # │░░▀\░░▀░░▀▀▀░▀▀▀░▀▀▀│
 # └────────────────────┘
-from libqtile import backend, bar, layout, widget, hook
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-import os, subprocess, psutil
+import os, subprocess
 from qtile_extras import widget
 
 mod = "mod4"
@@ -28,29 +28,28 @@ dracula = {
 # Bindings
 ############
 keys = [
-    # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "Tab", lazy.layout.next(), desc="Focus next window"),
     Key([mod, "shift"], "Tab", lazy.layout.previous(), desc="Focus previous window"),
-    # Move windows between left/right columns or move up/down in current stack.
+
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows.
+
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     Key([mod], "space", lazy.window.toggle_floating(), desc="Float window"),
     Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload config"),
     Key([mod, "shift"], "e", lazy.spawn("/home/aman/.local/bin/eww open --toggle powermenu"), desc="Powermenu"),
-    Key([mod], "r", lazy.spawn("rofi -show run -disable-history"), desc="Rofi"), # Rofi
+    Key([mod], "r", lazy.spawn("rofi -show run"), desc="Rofi"), # Rofi
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Fullscreen window"),
     # Volume Control
@@ -64,10 +63,10 @@ keys = [
 ##########
 groups = [
     Group("1", matches = [Match(wm_class="brave-browser")], label = "", layout = "max"),
-    Group("2", matches = [Match(wm_class=["kitty"])], label = "",),
-    Group("3", matches = [Match(wm_class="deadbeef")], label = "" ,layout = "max"),
-    Group("4", matches = [Match(wm_class="Steam")], label = "",layout = "max"),
-    Group("5", label = "", layout = "treetab"),
+    Group("2", matches = [Match(wm_class=["kitty", "neovide"])], label = "",),
+    Group("3", matches = [Match(wm_class="Deadbeef")], label = "", layout = 'max' ),
+    Group("4", matches = [Match(wm_class=["Steam", "lutris"]),Match(title="Steam")], label = "",layout = "max"),
+    Group("5", matches = [Match(wm_class="Thunar")], label = "", layout = "columns"),
 ]
 for i in groups:
     keys.extend([
@@ -81,34 +80,34 @@ for i in groups:
 layout_themes = {
     'border_width': 3,
     'border_focus': dracula['select'],
-    'border_normal': dracula['bg'],
+    'border_normal': dracula['grey'],
     'margin': 3,
 }
 layouts = [
-    layout.MonadTall(**layout_themes,ratio=0.6,max_ratio=0.8,min_ratio=0.3),
-    layout.TreeTab( panel_width=103,fontsize=10,previous_on_rm=True,vspace=-1,padding_x=0,font='JetbrainsMonoSemibold',margin_x=0,
-                    sections=['Windows'],section_left=25,
-                    # Colors
-                    **layout_themes,
-                    active_bg=dracula['select'],inactive_bg=dracula['grey'],bg_color=dracula['grey'],urgent_bg=dracula['red'],urgent_fg=dracula['fg'],
-                    active_fg=dracula['fg'],inactive_fg=dracula['fg'],section_fg=dracula['fg'],),
-    layout.Max(**layout_themes)
+    layout.MonadTall(**layout_themes,ratio=0.65,max_ratio=0.8,min_ratio=0.3),
+    layout.TreeTab(panel_width=103,fontsize=10,previous_on_rm=True,vspace=-1,padding_x=0,font='JetbrainsMonoSemibold',margin_x=0,
+        sections=['Windows'],section_left=25,
+        # Colors
+        **layout_themes,
+        active_bg=dracula['select'],inactive_bg=dracula['grey'],bg_color=dracula['grey'],urgent_bg=dracula['red'],urgent_fg=dracula['fg'],
+        active_fg=dracula['fg'],inactive_fg=dracula['fg'],section_fg=dracula['fg'],),
+    layout.Max(**layout_themes),
     # layout.Stack(num_stacks=1,**layout_themes),
     # layout.VerticalTile(),
     # layout.Zoomy(),
-    # layout.Columns(border_focus_stack=["#bd93f9", "#6272a4"], border_width=4),
+    layout.Columns(**layout_themes,border_focus_stack=["#bd93f9", "#6272a4"]),
     # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
 ]
-floating_layout = layout.Floating(**layout_themes,
+floating_layout = layout.Floating(
+    border_width = 3, border_focus = dracula['cyan'], border_normal = dracula['grey'], margin = 3,
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),
-        Match(wm_class="makebranch"),
+        Match(title="qtapp"),
         Match(wm_class="maketag"),
         Match(wm_class="ssh-askpass"),
         Match(title="branchdialog"),
@@ -143,21 +142,23 @@ screens = [
         bottom=bar.Bar(
             [
                 widget.GroupBox(
-                    border_width=2,disable_drag=True,padding=2,hide_unused=True,highlight_method="line",
-                    highlight_color=[dracula['grey'],dracula['grey']],inactive=dracula['fg'],this_current_screen_border=dracula['purple'],urgent=dracula['red']),
+                    disable_drag=True,padding=2,margin_x=1,hide_unused=True,highlight_method="line",fontsize=14,highlight_size=1,
+                    highlight_color=[dracula['grey'],dracula['grey']],inactive=dracula['fg'],this_current_screen_border=dracula['cyan'],urgent=dracula['red']),
                 widget.WindowName(
                     fontsize=11,font='JetbrainsMonoNerdFont',**powerline),
                 widget.TextBox(
                     text='ewwidgets',foreground=dracula['bg'],background=dracula['purple'],**powerline,
                     mouse_callbacks={'Button1': lazy.spawn('/home/aman/.local/bin/eww open-many --toggle stats music apps keybind')}),
                 widget.Wttr(
-                    foreground=dracula['cyan'],location={'kochi': 'Home'},format='%C %f (%t)',update_interval=10,**powerline),
+                    foreground=dracula['cyan'],location={'kochi': 'Home'},format='%c%C %f (actual %t)',update_interval=100,**powerline),
                 widget.Memory(
                     format='RAM:{MemUsed:.0f}{mm}/{MemTotal:.0f}{mm}',background=dracula['select'],**powerline),
                 widget.PulseVolume(
-                    fmt='{}',step=1,background=dracula['green'],foreground=dracula['bg'],**powerline,),
+                    fmt='{}',step=1,background=dracula['yellow'],foreground=dracula['bg'],**powerline,),
                 widget.UPowerWidget(
-                    update_interval=5,),
+                    text_charging='({percentage:.0f}%)',text_discharging='({percentage:.0f}%)',
+                    border_charge_colour=dracula['green'],border_colour=dracula['yellow'],border_critical=dracula['red'],
+                    fill_critical=dracula['red'],fill_low=dracula['yellow'],fill_normal=dracula['fg']),
                 widget.Clock(
                     format='%-d|%b|%Y(%a) %H:%M:%S',foreground=dracula['cyan'],**powerline,),
                 widget.Systray(
@@ -168,8 +169,9 @@ screens = [
             24,
             background=dracula['bg'],
             foreground=dracula['fg'],
-            border_width=[2, 2, 1, 2], # Up, Right, Down, Left
-            border_color=[dracula['cyan'], dracula['cyan'], dracula['cyan'], dracula['cyan']],
+            margin=[0,3,3,3],
+            border_width=[3, 2, 3, 2], # Up, Right, Down, Left
+            border_color=[dracula['bg'], dracula['bg'], dracula['bg'], dracula['bg']],
         ),
     ),
 ]
@@ -200,20 +202,3 @@ def set_floating(window):
     if (window.window.get_wm_transient_for()
             or window.window.get_wm_type() in floating_types):
         window.floating = True
-
-@hook.subscribe.client_new
-def _swallow(window):
-    pid = window.window.get_net_wm_pid()
-    ppid = psutil.Process(pid).ppid()
-    cpids = {
-        c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()
-    }
-    for i in range(5):
-        if not ppid:
-            return
-        if ppid in cpids:
-            parent = window.qtile.windows_map.get(cpids[ppid])
-            parent.minimized = True
-            window.parent = parent
-            return
-        ppid = psutil.Process(ppid).ppid()
